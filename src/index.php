0454430b2a8a8dc239a47b05cca1a52f44e2c340
Nathanaël Spriet <nathanael.spriet@gmail.com>
2018-04-06 15:09:05 +0200

    Add forms and a begining of a grid layout

--- src/index.php
+++ src/index.php
@@ -4,8 +4,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
+    <div class="grid_wrapper">
+        <div class="grid_one"></div>
+        <div class="grid_two">
+            <h1>Accueil</h1>
+            <form id="connexion_form" action="login" method="post">
+                <h2>Connexion</h2>
+                <p>
+                    <label for="username">Nom d'utilisateur :</label>
+                    <input type="text" name="username" id="connexion_username_input">
+                </p>
+                <p>
+                    <label for="password">Mot de passe :</label>
+                    <input type="password" name="password" id="connexion_password_input">
+                </p>
+                <p>
+                    <button type="submit">Se connecter</button>
+                </p>
+            </form>
+            <form id="search_form" action="search" method="post">
+                <h2>Recherche</h2>
+                <p>
+                    <label for="name">Nom :</label>
+                    <input type="text" name="name" id="search_name_input">
+                </p>
+                <p>
+                    <button type="submit">Chercher</button>
+                </p>
+            </form>
+        </div>
+        <div class="grid_three"></div>
+    </div>
+
 </body>
 </html>
