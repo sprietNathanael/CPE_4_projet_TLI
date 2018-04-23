ecdcba8c56e9d739b13adbb709c98d17c2cfe34b
Nathanaël Spriet <nathanael.spriet@gmail.com>
2018-04-23 14:41:58 +0200

    Begin the column styling

--- src/index.php
+++ src/index.php
@@ -5,13 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>Accu</title>
 </head>
 <body>
+    <div id="titleBar" class="grid_wrapper">
+        <h1>Accueil</h1>
+    </div>
     <div class="grid_wrapper">
         <div class="grid_one"></div>
         <div class="grid_two">
-            <h1>Accueil</h1>
             <form id="connexion_form" action="login" method="post">
                 <h2>Connexion</h2>
                 <p>
