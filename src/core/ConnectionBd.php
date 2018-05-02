5692fa8aec375879e44bf2f91c6a6878cf2e48c3
Unknown <thomas.cordier@cpe.fr>
2018-05-02 14:50:33 +0200

    gestion utf8 bd

--- src/core/ConnectionBd.php
+++ src/core/ConnectionBd.php
@@ -13,7 +13,7 @@ class ConnectionBd
         try {
             $user = 'root';
             $pass = '';
-            $conn = new PDO('mysql:host=localhost;dbname=acu', 'root', '');
+            $conn = new PDO('mysql:host=localhost;dbname=acu', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             // set the PDO error mode to exception
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
