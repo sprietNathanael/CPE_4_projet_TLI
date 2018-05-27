1a5c57cdc2914bbeb8a91fad328ed2ed2e2383cd
Unknown <tom-mollaret@orange.fr>
2018-05-27 17:58:40 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ src/core/ConnectionBd.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Thomas
+ * Date: 06/04/2018
+ * Time: 14:32
+ */
+
+class ConnectionBd
+{
+    public function getConnection()
+    {
+        try {
+            // set the PDO error mode to exception
+            $conn = new PDO('mysql:host=localhost;dbname=TLI', 'root', 'root', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+
+            return $conn;
+
+        } catch (PDOException $e) {
+            print "<Erreur !: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    }
+}
+
+
