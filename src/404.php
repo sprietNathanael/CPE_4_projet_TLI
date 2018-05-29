cb7706ffd4ba090aa201a4b7fc149493418facbc
dams200 <damiend2@hotmail.fr>
2018-05-29 06:35:05 -0400

    Add files via upload
    Page 404

---
+++ src/404.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	    <meta charset="UTF-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	    <title>Erreur 404</title>
+	</head>
+
+	<body>
+		<img src="src/assets/icons/404.jpg" alt="Photo-erreur-404" width="100%" height="100%" />
+	</body>
+</html>
