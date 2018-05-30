ac9228d5fa3e99f30a23db5ae9e2789d8387e4ef
Nathanaël Spriet <nathanael.spriet@gmail.com>
2018-05-30 21:55:17 +0200

    Make the search page work

--- src/search.php
+++ src/search.php
@@ -3,6 +3,6 @@ require_once "layout.php";
 
 $smartyTemplate->assign('currentPage', "recherche");
 $smartyTemplate->assign('stylesheets', ["diseases.css"]);
-$smartyTemplate->assign('scripts', ["search.js"]);
+$smartyTemplate->assign('scripts', ["search.js", "diseases.js", "jquery.min.js"]);
 $smartyTemplate->display("search.tpl");
 
